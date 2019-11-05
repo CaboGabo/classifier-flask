1dafdbea8750e6a9dea4b49388a93fca2d6586f4
Yahir Trejo <yahir.trejo.g@gmail.com>
2019-11-05 01:11:37 -0600

    production test

--- index.py
+++ index.py
@@ -39,3 +39,12 @@ def diagnosticate():
     response['topWords'] = words_used
 
     return jsonify(response)
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
